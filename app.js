589d379894d27c3276364eb3b0ff3e6d182a1316
richardpringle <rpring9@gmail.com>
2015-05-22 11:55:12 -0400

    going to write to program on my computer then make the serial and server adjustments for running on UDOO

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var exec = require('child_process').exec, child;
+// Requires
+var http = require('http');
+// var SerialPort = require("serialport").SerialPort;
 
-child = exec('cat /sys/class/gpio/gpio40/direction', function (error, stdout, stderr) {
-	console.log('stdout: ' + stdout);
-	console.log('stderr' + stderr);
-	if (error) {
-		console.log('exec error: ' + error)
-	}
-});
+// // Objects
+// var serial0 = new SerialPort("/dev/ttymxc3", {baudrate: 115200});
+
+// Create Server
+var server = http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello World!');
+}).listen(1337, '127.0.0.1'); // The IP address is subject to change depending on the network
+console.log('Server is running at http://142.157.114.70');
+
+// serial0.on("open", function () {
+//   console.log('open');
+//   serial0.on('data', function(data) {
+//     console.log('data received: ' + data);
+//   });
+//   serial0.write("\n", function(err, results) {
+//     console.log('err ' + err);
+//     console.log('results ' + results);
+//   });
+// });
