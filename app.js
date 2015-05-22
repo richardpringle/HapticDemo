f7c28dd67be8aca95dfcf614dfee42fe185e6cda
richardpringle <rpring9@gmail.com>
2015-05-22 12:32:04 -0400

    Right now the Serial port will open, write and recieve, and the server will also open and listen. What I want to do next is write and receive within the initial server creation.

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 // Requires
 var http = require('http');
-// var SerialPort = require("serialport").SerialPort;
+var SerialPort = require("serialport").SerialPort;
 
-// // Objects
+// Objects
 // var serial0 = new SerialPort("/dev/ttymxc3", {baudrate: 115200});
+var serial0 = new SerialPort("COM10", {baudrate: 115200});
 
 // Create Server
 var server = http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Hello World!');
-}).listen(1337, '127.0.0.1'); // The IP address is subject to change depending on the network
+// }).listen(1337, '142.157.114.70'); 
+}).listen(1337, '127.0.0.1'); 
 console.log('Server is running at http://142.157.114.70');
 
-// serial0.on("open", function () {
-//   console.log('open');
-//   serial0.on('data', function(data) {
-//     console.log('data received: ' + data);
-//   });
-//   serial0.write("\n", function(err, results) {
-//     console.log('err ' + err);
-//     console.log('results ' + results);
-//   });
-// });
+// When the serial port is opened 
+serial0.on("open", function () {
+  console.log('open');
+  serial0.on('data', function (data) {
+    console.log('data received: ' + data);
+  });
+  serial0.write("\n", function (err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+});
+
+serial0.write("\n", function (err, results) {
+	console.log('err ' + err);
+    console.log('results ' + results);
+});
+
+serial0.on('data', function (data) {
+    console.log('data received: ' + data);
+});
