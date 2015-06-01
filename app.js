7422c11edcfd417a59bfdcb1ed042f3f72399c9a
richardpringle <rpring9@gmail.com>
2015-06-01 13:03:48 -0400

    test1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var serialport = require('serialport');
 var util = require('util');
 
 var SerialPort = serialport.SerialPort;
-var serial0 = new SerialPort("COM10", {baudrate: 115200, parser: serialport.parsers.raw});
+var serial0 = new SerialPort("/dev/ttymxc3", {baudrate: 115200, parser: serialport.parsers.raw});
 
 
 // // The code below works. Once the serial port is opened, the server is created.
@@ -37,31 +37,7 @@ bufForce = Buffer.concat(buff_xy);
 
 var allData;
 
-// // The following code works for receiving the position and velocity data
-// serial0.on('open', function () {
-// 	console.log('open');
-// 		serial0.write(bufForce, function(err, data) {
-// 			console.log('resultsOut ' + data);
-// 			if (err) {
-// 				console.error(err);
-// 			}
-// 		});
-// 	serial0.on('data', function(data) { 
-// 		allData = 	[
-// 						data.slice(0,4).readFloatLE(),
-// 						data.slice(4,8).readFloatLE(),
-// 						data.slice(8,12).readFloatLE(),
-// 						data.slice(12,16).readFloatLE()
-// 					]
-// 		console.log(allData);
-// 		serial0.write(bufForce, function(err, data) {
-// 			if (err) {
-// 				console.error(err);
-// 			}
-// 		});
-// 	});
-// });
-
+// The following code works for receiving the position and velocity data
 serial0.on('open', function () {
 	console.log('open');
 		serial0.write(bufForce, function(err, data) {
@@ -71,6 +47,17 @@ serial0.on('open', function () {
 			}
 		});
 	serial0.on('data', function(data) { 
-		console.log("data: " + data);
+		allData = 	[
+						data.slice(0,4).readFloatLE(),
+						data.slice(4,8).readFloatLE(),
+						data.slice(8,12).readFloatLE(),
+						data.slice(12,16).readFloatLE()
+					]
+		console.log(allData);
+		serial0.write(bufForce, function(err, data) {
+			if (err) {
+				console.error(err);
+			}
+		});
 	});
 });
