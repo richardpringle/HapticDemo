9a4900aad2a629d120dde47baed192b8ebceaa3d
richardpringle <rpring9@gmail.com>
2015-06-01 13:07:42 -0400

    test2

--- app.js
+++ app.js
@@ -3,26 +3,7 @@ var serialport = require('serialport');
 var util = require('util');
 
 var SerialPort = serialport.SerialPort;
-var serial0 = new SerialPort("/dev/ttymxc3", {baudrate: 115200, parser: serialport.parsers.raw});
-
-
-// // The code below works. Once the serial port is opened, the server is created.
-// // Inside you can receive and send serial commands, passing them to the client.
-// serial0.on("open", function () {
-// 	console.log('open');
-// 	http.createServer(function (req, res) {
-// 		res.writeHead(200, {'Content-Type': 'text/plain'});
-// 		if (req.method === 'GET') {
-// 			serial0.write(0x, function(err, data) {
-// 				console.log('results ' + data);
-// 			});
-// 			serial0.on('data', function(data) {
-// 				console.log('data received: ' + data);
-// 				res.end(data);
-// 			});
-// 		}
-// 	}).listen(1000);
-// });
+var serial0 = new SerialPort("/dev/ttymxc3", {baudrate: 115200});
 
 var fx = 0.0;
 var fy = 999999999999.0;
