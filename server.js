7600342b283f107164077b5a7e073ba16bce8f16
richardpringle <rpring9@gmail.com>
2015-06-07 15:56:08 -0400

    test2 of getting the server back on linux

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
-// Requirements
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
+// // Requirements
+// var express = require('express');
+// var app = express();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+// var path = require('path');
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
-app.get('/', function (req, res) {
-	// res.sendFile(indexPath);
-	res.sendFile('index.html');
-});
+// app.get('/', function (req, res) {
+// 	// res.sendFile(indexPath);
+// 	res.sendFile('index.html');
+// });
 
-io.on('connection', function(socket){
+// io.on('connection', function(socket){
 	
-	console.log('a user connected');
+// 	console.log('a user connected');
 
-  	socket.on('disconnect', function(){
-    	console.log('user disconnected');
-  	});
+//   	socket.on('disconnect', function(){
+//     	console.log('user disconnected');
+//   	});
 
-});
+// });
 
-http.listen(1000, '142.157.114.55', function(){
-  console.log('server running at 142.157.114.55')
-  console.log('listening on *:1000');
-});
+// http.listen(1000, '142.157.114.55', function(){
+//   console.log('server running at 142.157.114.55')
+//   console.log('listening on *:1000');
+// });
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'content-type': 'text/plain'});
+	res.end('Hello World2!');
+}).listen(1000, '142.157.114.55');
 
 
 
